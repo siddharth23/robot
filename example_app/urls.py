d30293d9bea7a16ca099d1dce25c78ef32edf3d6
Siddharth Kala <siddharth.kala1989@gmail.com>
2018-06-18 09:30:02 +0530

    Update urls.py

--- example_app/urls.py
+++ example_app/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
 from example_app.views import ChatterBotAppView
-from example_app.views import calculate_view,eslc_view
+from example_app.views import command_view_new
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
-    url(r'^calculate',calculate_view),
-    url(r'^eslccommand',eslc_view),
+    url(r'^commands', command_view_new),
 
 ]
