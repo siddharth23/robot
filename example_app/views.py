799b3ec7a2c76d3ccbf5eb03c88623ba88381bd0
Siddharth Kala <siddharth.kala1989@gmail.com>
2018-06-18 09:30:28 +0530

    Update views.py

--- example_app/views.py
+++ example_app/views.py
@@ -2,31 +2,34 @@ from django.views.generic.base import TemplateView
 from django.http import JsonResponse
 from datetime import datetime
 import requests
+from chatterbot.trainers import ListTrainer
 from urllib.parse import unquote
+from chatterbot import ChatBot
+
+bot = ChatBot("Shiv")
+bot.set_trainer(ListTrainer)
+file_data = open("example_app/static/training_data", "r").readlines()
+bot.train(file_data)
+bot2 = ChatBot("Sid", logic_adapters=[
+    "chatterbot.logic.TimeLogicAdapter"
+])
+
+
 class ChatterBotAppView(TemplateView):
     template_name = "app.html"
+
 
 def replace_all(text, dic):
     for i, j in dic.items():
         text = text.replace(i, j)
     return text
 
-def calculate_view(request, *args, **kwargs):
-    reqs=unquote(request.GET.get('value'))
-    print(reqs)
-    t={"into":"*","cross":"*","multiply":"*","divide":"/","divide by":"/","divided by":"/","divided":"/","power":"**","square root":"** 0.5","cube root":"** round(float(1/3),2)"}
-    sts=replace_all(reqs,t)
-    x=""
-    if(reqs.startswith('time')):
-        x=eval("datetime."+reqs.replace("time","").replace(" ","")+"()")
-    else:
-        x=eval(sts)
-    return JsonResponse({'result':str(x)})
 
-def eslc_view(request, *args, **kwargs):
+def command_view_new(request, *args, **kwargs):
     reqs = unquote(request.GET.get('value'))
-    resp=requests.get("https://reqres.in/api/users/2")
-    if resp.status_code == 200:
-        return JsonResponse({'result': str(reqs)+" successful"})
+    print(reqs)
+    response = bot.get_response(reqs)
+    if response.confidence > 0.7:
+        return JsonResponse({"value": str(response)})
     else:
-        return JsonResponse({'result': str(reqs) + " not successful"})
+        return JsonResponse({"value": "Sorry can't understand you"})
